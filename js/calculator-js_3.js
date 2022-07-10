fc119c45f362ca71c2047faf09bd8bc23440d3ab
Ravi M.P <madscience@hush.com>
2022-07-10 11:31:24 -0300

    correct typo: from cont to count

--- js/calculator-js_3.js
+++ js/calculator-js_3.js
@@ -3,19 +3,19 @@ console.log('running_cal_3');
 numbersArr = [[],[]];
 console.log(numbersArr);
 
-let cont = 0;
+let count = 0;
 let final = 0;
 let firstSubArrAdded  = '';
 let secondSubArrAdded = '';
 function equalsButton() {
     for(let i = 0; i < numbersArr[0].length; i++) {
-            firstSubArrAdded += numbersArr[0][i];
-            console.log(firstSubArrAdded);
+        firstSubArrAdded += numbersArr[0][i];
+        console.log(firstSubArrAdded);
         
     };
     for(let j = 0; j < numbersArr[1].length; j++) {
-            secondSubArrAdded += numbersArr[1][j];
-            console.log(secondSubArrAdded);
+        secondSubArrAdded += numbersArr[1][j];
+        console.log(secondSubArrAdded);
         
     };
     
@@ -27,22 +27,22 @@ function equalsButton() {
 };
 
 function addButton() {
-    cont++
+    count++
     final = (Number(firstSubArrAdded) + Number(secondSubArrAdded));
     console.log(Number(final.toFixed(2)));
 };
 function subtractButton() {
-    cont++
+    count++
     final = (Number(firstSubArrAdded) - Number(secondSubArrAdded));
     console.log(Number(final.toFixed(2)));
 };
 function multiplyButton() {
-    cont++
+    count++
     final = (Number(firstSubArrAdded) * Number(secondSubArrAdded));
     console.log(Number(final.toFixed(2)));
 };
 function divideButton() {
-    cont++
+    count++
     final = (Number(firstSubArrAdded) / Number(secondSubArrAdded));
     console.log(Number(final.toFixed(2)));
 };
@@ -54,36 +54,36 @@ function cButton() {
 };
 
 function numberOneButton() {
-    numbersArr[cont].push('1');
+    numbersArr[count].push('1');
 };
 function numberTwoButton() {
-    numbersArr[cont].push('2');
+    numbersArr[count].push('2');
 };
 function numberThreeButton() {
-    numbersArr[cont].push('3');
+    numbersArr[count].push('3');
 };
 function numberFourButton() {
-    numbersArr[cont].push('4');
+    numbersArr[count].push('4');
 };
 function numberFiveButton() {
-    numbersArr[cont].push('5');
+    numbersArr[count].push('5');
 };
 function numberSixButton() {
-    numbersArr[cont].push('6');
+    numbersArr[count].push('6');
 };
 function numberSevenButton() {
-    numbersArr[cont].push('7');
+    numbersArr[count].push('7');
 };
 function numberEightButton() {
-    numbersArr[cont].push('8');
+    numbersArr[count].push('8');
 };
 function numberNineButton() {
-    numbersArr[cont].push('9');
+    numbersArr[count].push('9');
 };
 function numberZeroButton() {
-    numbersArr[cont].push('0');
+    numbersArr[count].push('0');
 };
 function periodButton() {
-    numbersArr[cont].push('.');
+    numbersArr[count].push('.');
 };
 
